b4ee5b76f0e1f657cb161b7b7e5065e1f773cc83
Francesco Casetta <francesco.casetta@gmail.com>
2024-10-20 11:45:02 +0200

    chi router and middleware

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", basicHandler)
+
+	server := &http.Server{
+		Addr: ":3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		fmt.Println("Error starting server", err)
+	} else {
+		fmt.Println("Server started on port :3000")
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, world!"))
+}
